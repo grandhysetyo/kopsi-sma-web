f986bc73ef9eba6424b7f87070814e552320dd82
Grandhys Setyo Utomo <setyo.grandhys1@gmail.com>
2021-05-21 14:07:19 +0700

    add project kopsi sma react laravel

---
+++ resources/js/index.js
@@ -0,0 +1,55 @@
+import {useState, useEffect} from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route  
+} from "react-router-dom";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+import Navbar from "./components/Navbar";
+import Beranda from "./pages/Beranda";
+import Bidang from "./pages/Bidang";
+import Login from "./pages/Login";
+
+// Style
+import '../sass/style.scss';
+
+// 0 Light
+// 1 Dark
+
+function App() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+  const [nav, setNav] = useState(true);
+  const handleSectionChange = (section) => {
+    if(section===0){
+      setNav(true)
+    }else{
+      setNav(false)
+    }
+  }
+  
+  return (
+    <>
+    <Router>      
+      <Navbar isDark={nav}/>
+      <Switch>
+        <Route path="/" exact>
+          <Beranda sectionActive={handleSectionChange} />
+        </Route>
+        <Route path="/bidang" exact>          
+          <Bidang sectionActive={handleSectionChange}/>
+        </Route>
+        <Route path="/login" exact>
+          <Login sectionActive={handleSectionChange}/>
+        </Route>
+      </Switch>
+    </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ resources/views/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>KOPSI | PUSPRESNAS 2021</title>
+        <link rel="icon" type="image/png" href="./assets/images/favicon.png" />
+       
+        <!-- Styles -->
+        <link rel="stylesheet" href="./css/app.css" />        
+
+         <!-- Font Awesome -->
+        <script src="https://kit.fontawesome.com/1e6b6f692c.js" crossorigin="anonymous"></script>
+        
+        
+    </head>
+    <body>
+        <div id="root"></div>
+
+        <script src="./js/app.js" > </script>        
+    </body>
+</html>
